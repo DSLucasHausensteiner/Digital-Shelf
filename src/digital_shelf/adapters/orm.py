d7c2aba678edad343b27bca6298baa1df4da06e0
LucasHausi <l.hausensteiner@gmail.com>
2025-11-20 16:41:34 +0100

    change the orm and model to new schema

--- src/digital_shelf/adapters/orm.py
+++ src/digital_shelf/adapters/orm.py
@@ -1,6 +1,6 @@
 from digital_shelf.domain import model
 
-from sqlalchemy import Column, Integer, String, Table, DateTime, UniqueConstraint
+from sqlalchemy import Column, Integer, Float, String, Table, DateTime, UniqueConstraint
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import registry
 
@@ -11,12 +11,19 @@ products = Table(
     "products",
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("name", String(255)),
+    Column("name", String(255), nullable=False),
     Column("qty", Integer, nullable=False),
-    Column("expiry_date", DateTime, nullable=False),
+    Column("size_amount", Float, nullable=True),
+    Column("size_unit", String("10"), nullable=True),
+    Column("expiry_date", DateTime, nullable=True),
     Column("nutrition_facts", JSONB, nullable=True),
-    UniqueConstraint("name","expiry_date", name="uq_product_name_expiry")
+    UniqueConstraint(
+        "name", "expiry_date", "size_amount", "size_unit",
+        name="uq_product_unique_item"
+    )
 )
 
 def start_mappers():
     mapper_registry.map_imperatively(model.Product, products)
+
+start_mappers()
--- src/digital_shelf/domain/model.py
+++ src/digital_shelf/domain/model.py
@@ -1,9 +1,16 @@
+from typing import Optional
 from dataclasses import dataclass
 from pydantic import BaseModel
 from datetime import datetime
 
+class Unit(BaseModel):
+    amount: Optional[float] = None
+    unit: Optional[str] = None
+
+
 class Product(BaseModel):
-    name: str
-    qty: int
-    expiry_date: datetime
-    nutrition_facts: dict
+    name: Optional[str] = None
+    qty: Optional[int] = None
+    size: Optional[Unit] = None
+    expiry_date: Optional[datetime] = None
+    nutrition_facts:Optional[dict] = {}
